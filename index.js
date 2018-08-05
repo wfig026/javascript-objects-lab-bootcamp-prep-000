ff86fa169ccddfad555098f91006bbe659b5071c
Will Figueiredo <will@wfigueiredo.com>
2018-08-05 18:04:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var recipes = {
-  eggs: 3,
-  flour: '2 cups',
-  milk: '1 cup'
+var recipes = new Object();
+ function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},object,{[key]: value})
 }
-
-function updateObjectWithKeyAndValue(object, key, value) {
-  
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+ object[key] = value
+return object
+}
+ function deleteFromObjectByKey(object, key){
+  var newObj =  Object.assign({},object)
+  delete newObj[key]
+  return newObj
+ }
+ function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
 }
